dc039a6f0be953e1a9923edcef3edef833fed91c
Evgeniy <eurbagayev@mail.com>
2022-09-20 23:05:00 +0300

    Add При вводе числа выводит последовательность от -n до n

---
+++ Task_5/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.Write("Введите число:  ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = -num1;
+
+while(num2 <= num1)
+{
+    Console.Write($"{num2}");
+    num2++;
+}
