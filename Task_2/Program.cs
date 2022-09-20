51423d4cd00863ccb40441d01e6757f3fef0f561
Evgeniy <eurbagayev@mail.com>
2022-09-20 20:30:36 +0300

    Add Является ли 1 число квадратом 2

---
+++ Task_2/Program.cs
@@ -0,0 +1,14 @@
+﻿
+Console.Clear();
+Console.Write("Введите число:  ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine());
+int result = num2 * num2;
+if(num1 == result)
+{
+    Console.WriteLine($" {num1} является квадратом {num2}");
+}
+else
+{
+    Console.WriteLine($" {num1} не является квадратом {num2}");
+}
