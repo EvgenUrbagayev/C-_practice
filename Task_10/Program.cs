8d00dcb63f08dbb293bae5796375da09f4e8cd71
Evgeniy <eurbagayev@mail.com>
2022-09-21 20:37:00 +0300

    Add проверка является ли вводимое число квадратом друго числа

---
+++ Task_10/Program.cs
@@ -0,0 +1,23 @@
+﻿using static System.Console;
+
+Clear();
+Write("Введите число:  ");
+int num = Convert.ToInt32(Console.ReadLine());
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+if(num == num1 * num1)
+{
+    Write($"{num} является квадратом {num1}");
+}
+else
+{
+if(num1 == num * num)
+{
+    Write($"{num1} является квадратом {num}");
+}
+
+else
+{
+    Write($"{num1} не является квадратом {num} и {num} не является квадратом {num1}");
+}
+}
