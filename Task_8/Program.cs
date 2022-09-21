c5c640f9c4f5523b4247491ef5218b3f1b54cffc
Evgeniy <eurbagayev@mail.com>
2022-09-21 20:00:33 +0300

    Add кратные числа

---
+++ Task_8/Program.cs
@@ -0,0 +1,16 @@
+﻿using static System.Console;
+
+Clear();
+Write("Введите число:  ");
+int num = Convert.ToInt32(Console.ReadLine());
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+if(num % num1 == 0)
+{
+    Write($"{num1} является кратным {num}");
+}
+else
+{
+    int num2 = num % num1;
+    Write($"Остаток от деления {num2}");
+}
