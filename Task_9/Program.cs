fbfb567cd64779a7d15e9222d2e1544480c8bc5a
Evgeniy <eurbagayev@mail.com>
2022-09-21 20:13:09 +0300

    Add проверка вводимого числа на кратность 7 и 23

---
+++ Task_9/Program.cs
@@ -0,0 +1,16 @@
+﻿using static System.Console;
+
+Clear();
+Write("Введите число:  ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if(num % 7 == 0)
+{
+    if(num % 23 == 0)
+    Write($"{num} кратно числам 7 и 23");
+
+}
+else
+{
+    Write($"{num} не является кратно числам 7 и 23");
+}
