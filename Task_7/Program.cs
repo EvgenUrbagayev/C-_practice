bcac1ab5fc0a94ce1909bd0b917477d4ccda9f78
Evgeniy <eurbagayev@mail.com>
2022-09-21 19:44:08 +0300

    Add при вводе трехзначного числа удаляем вторую цифру

---
+++ Task_7/Program.cs
@@ -0,0 +1,10 @@
+﻿using static System.Console;
+
+Clear();
+//Write("Введите число:  ");
+int number = new Random().Next(100, 999+1); // генерируем число в указанном диапазоне
+WriteLine(number);  // выводим число в консоль
+int num = number / 100;  // забираем старший разряд
+int num1 = number % 10;  // забираем младший разряд
+
+WriteLine($"{num}{num1}");
