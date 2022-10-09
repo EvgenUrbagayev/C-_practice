c546bdec1062afa2922a3b024960b4e4ac7e7398
Evgeniy <eurbagayev@mail.com>
2022-10-09 12:51:25 +0300

    Замена элементов массива положит на отрицат и наоборот

---
+++ Seminar_5/Задача_2/Program.cs
@@ -0,0 +1,52 @@
+﻿// Задача: напишите программу, которая меняет элементы массива положительные
+// на отрицательные и наоборот.
+
+
+
+
+using static System.Console;
+Clear();
+WriteLine("Введите размер массива:  ");
+int lenght = int.Parse(ReadLine()!);
+WriteLine("Введите минимальное значение массива:  ");
+int min = int.Parse(ReadLine()!);
+WriteLine("Введите максимальное значение массива:  ");
+int max = int.Parse(ReadLine()!);
+
+
+int[] array = GetArray(lenght, min, max); 
+Write($"Исходный массив: [{String.Join(",", array)}]");
+
+
+
+for(int i = 0; i < array.Length; i++)
+{
+    if(array[i] > 0)
+    {
+        array[i] = - array[i];
+    }
+    else
+    {
+        array[i] = - array[i];
+    }
+       
+}
+WriteLine();
+Write($"Измененный массив: [{String.Join(",", array)}]");
+
+
+int[] GetArray(int size, int minValue, int maxValue)
+
+{
+    Random rnd = new Random();
+    int[] result = new int[size];
+    for(int i = 0; i < result.Length; i++)
+    {
+        result[i] = rnd.Next(minValue, maxValue+1);
+    }
+
+
+
+    return result;
+
+}
